a8dd7e26cf8616aaa9107863b840813c22abf116
Fox <filip.debczak@gmail.com>
2019-07-06 15:26:26 +0100

    commit 2

---
+++ graphics.h
@@ -0,0 +1,44 @@
+#ifndef _GRAPHICS_H
+#define _GRAPHICS_H
+#include <SFML/Graphics.hpp>
+
+class Graphics {
+
+public:
+
+	static int const SCREEN_WIDTH = 600;
+	static int const SCREEN_HEIGHT = 350;
+	const char* WINDOW_TITLE = "Fox-Player";
+	const sf::Color WINDOW_COLOR = sf::Color::White;
+
+private:
+
+	static Graphics* sInstance;
+	static bool sInitialized;
+
+	sf::RenderWindow* mWindow;
+
+	sf::Event* mEvents;
+
+public:
+
+	static Graphics* Instance();
+	static void Release();
+	static void Initialized();
+
+	void ClearBackBuffer();
+
+	sf::Window* GetRenderWindow();
+
+	void Render();
+
+private:
+
+	Graphics();
+	~Graphics();
+
+	bool Init();
+	
+};
+
+#endif
